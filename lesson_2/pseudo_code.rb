ddd3a92e5ce10662fa0a4876911584f430f46529
Monte Amador <monteamador@me.com>
2021-01-20 13:03:57 -0800

    add lesson 1 and 2

---
+++ lesson_2/pseudo_code.rb
@@ -0,0 +1,57 @@
+# 1) a method that returns the sum of two integers (casual)
+# define two number
+# add both numbers together
+# return the sum of both numbers
+
+# START
+
+# 1) a method that returns the sum of two integers (Formal)
+# SET number1 = GET user first number
+# SET number2 = GET user second number
+# PRINT number1.to_i + number2.to_i
+
+# END
+
+***
+
+# 2) a method that takes an array of strings, and returns a string that is all those strings concatenated together (casual)
+
+# Define strings
+# Save strings to array
+# iterate through array and save each index as a variable
+# print and concatenate each variable
+
+# 2) a method that takes an array of strings, and returns a string that is all those strings concatenated together (formal)
+
+# START
+
+# SET string1 = "string1"
+# SET string2 = "string2"
+# SET string3 = "string3"
+
+# SET array = []
+# array.push(string1)
+# array.push(string2)
+# array.push(string3)
+
+# ITERATE array and assign each index to variable
+# PRINT each variable concatenated to the next variable
+
+# END
+
+***
+
+# 3) a method that takes an array of integers, and returns a new array with every other element (casual)
+
+# create an array of integers
+# iterate through the array and print odd indexes to new array
+
+
+# 3) a method that takes an array of integers, and returns a new array with every other element (formal)
+
+# START
+
+# SET array_of_integers = [10, 20, 30, 5, 13, 4]
+# ITERATE through array_of_integers and map odd indexes
+
+# END
