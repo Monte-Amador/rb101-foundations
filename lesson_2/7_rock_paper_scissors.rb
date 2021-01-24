e77793a6a145d4d247d86b04a536ffd22a8dc4ff
Monte Amador <monteamador@me.com>
2021-01-23 20:07:11 -0800

    refactor: move logic into own method

--- lesson_2/7_rock_paper_scissors.rb
+++ lesson_2/7_rock_paper_scissors.rb
@@ -4,6 +4,19 @@ def prompt(message)
   Kernel.puts(">> #{message}")
 end
 
+def display_results(player, computer)
+  if (player == "Rock" && computer == "Scissors") || (player == "Scissors" && computer == "Paper") || (player == "Paper" && computer == "Rock")
+    puts "You won!"
+  
+  elsif (player == "Rock" && computer == "Rock") || (player == "Scissors" && computer == "Scissors") || (player == "Paper" && computer == "Paper")
+    puts "It's a tie!"
+  
+  else
+    puts "The computer won!"
+  end
+end
+
+
 prompt("Welcome to #{VALID_CHOICES}!")
 
 loop do # main loop
@@ -11,35 +24,23 @@ loop do # main loop
 
   loop do # verification loop
     prompt("Please choose one: #{VALID_CHOICES}.")
-    user_choice = Kernel.gets().chomp()
+    user_choice = Kernel.gets().chomp().capitalize
 
-    # lowercase_sensitive_input = VALID_CHOICES.map {|str| str.downcase}
-    # uppercase_sensitive_input = VALID_CHOICES.map {|str| str.upcase}
-
-    if ( VALID_CHOICES.include?("#{user_choice}".capitalize) )
+    if ( VALID_CHOICES.include?("#{user_choice}") )
       break
     else
       puts "Sorry, that is not a valid choice, let's try again."
     end
   end
 
-
   computer_choice = VALID_CHOICES.sample()
-  prompt("You chose \"#{user_choice.capitalize}\" and the computer chose \"#{computer_choice}\":")
+  
+  prompt("You chose \"#{user_choice}\" and the computer chose \"#{computer_choice}\":")
 
-  if (user_choice.capitalize == "Rock" && computer_choice == "Scissors") || (user_choice.capitalize == "Scissors" && computer_choice == "Paper") || (user_choice.capitalize == "Paper" && computer_choice == "Rock")
-    puts "You won!"
-  elsif (user_choice.capitalize == "Rock" && computer_choice == "Rock") || (user_choice.capitalize == "Scissors" && computer_choice == "Scissors") || (user_choice.capitalize == "Paper" && computer_choice == "Paper")
-    puts "It's a tie!"
-  else
-    puts "The computer won!"
-  end
+  display_results(user_choice, computer_choice)
 
   prompt("Would you like to play again? (y to play again)")
   answer = Kernel.gets().chomp().downcase
   
   break unless answer == 'y'
 end
-
-# refactor:
-# consolidate
