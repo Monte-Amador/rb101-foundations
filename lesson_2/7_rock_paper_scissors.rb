7052ed0d1e19adad9e7a1a8747cbc2b101303d11
Monte Amador <monteamador@me.com>
2021-01-24 16:20:37 -0800

    refactor: basic cleanup with improved and shorter logic for the display_results method

--- lesson_2/7_rock_paper_scissors.rb
+++ lesson_2/7_rock_paper_scissors.rb
@@ -4,45 +4,43 @@ def prompt(message)
   Kernel.puts(">> #{message}")
 end
 
+def win?(first, second)
+  (first == "Rock" && second == "Scissors") ||
+    (first == "Scissors" && second == "Paper") ||
+    (first == "Paper" && second == "Rock")
+end
+
 def display_results(player, computer)
-  if (player == "Rock" && computer == "Scissors") || (player == "Scissors" && computer == "Paper") || (player == "Paper" && computer == "Rock")
-    prompt "You won!"
-  
-  elsif (player == "Rock" && computer == "Rock") || (player == "Scissors" && computer == "Scissors") || (player == "Paper" && computer == "Paper")
-    prompt "It's a tie!"
-  
+  if win?(player, computer)
+    prompt("You won!")
+  elsif win?(computer, player)
+    prompt("The computer won!")
   else
-    prompt "The computer won!"
+    prompt("It's a tie!")
   end
 end
 
-
-
 prompt("Welcome to #{VALID_CHOICES.join(', ')}!")
-
 loop do # main loop
   user_choice = ''
-
+  
   loop do # verification loop
     prompt("Please choose one: #{VALID_CHOICES.join(', ')}.")
-    user_choice = Kernel.gets().chomp().capitalize
-
-    if ( VALID_CHOICES.include?("#{user_choice}") )
+    user_choice = Kernel.gets().chomp().capitalize()
+    if VALID_CHOICES.include?(user_choice)
       break
     else
-      puts "Sorry, that is not a valid choice, let's try again."
+      prompt("Sorry, that is not a valid choice, let's try again.")
     end
   end
 
   computer_choice = VALID_CHOICES.sample()
   
   prompt("You chose \"#{user_choice}\" and the computer chose \"#{computer_choice}\":")
-
   display_results(user_choice, computer_choice)
-
+  
   prompt("Would you like to play again? (y to play again)")
   answer = Kernel.gets().chomp().downcase
-  
   break unless answer == 'y'
 end
 
