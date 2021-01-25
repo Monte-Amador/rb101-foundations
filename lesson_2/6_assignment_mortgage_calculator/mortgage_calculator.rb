44498cc176b426535a71d0103d1f17c3950662d5
Monte Amador <monteamador@me.com>
2021-01-24 16:29:59 -0800

    refactor: general readability cleanup & preparing for logic consolidation

--- lesson_2/6_assignment_mortgage_calculator/mortgage_calculator.rb
+++ lesson_2/6_assignment_mortgage_calculator/mortgage_calculator.rb
@@ -1,3 +1,4 @@
+# 1. METHOD INITIALIZATIONS
 def prompt (message)
   puts ">> #{message}"
 end
@@ -15,16 +16,18 @@ def number?(input)
 end
 
 def monthly_interest_rate(apr,time)
-  rate = apr / time
+  mi_rate = apr / time
 end
 
+# 2. USER INPUT AND VALIDATION
 prompt("Welcome to the Monthly Loan Calculator! Please type in your name:")
 name = gets().chomp()
 prompt("Hello #{name}, Let's get started!")
-monthly_interest_rate = ''
+
 loop do # main loop
   prompt("How much would you like a loan for?")
   loan_amount = gets().chomp().to_f() 
+  
   prompt("Great, what is the Annual Percentage Rate (APR) of your loan?")
   apr = gets().chomp().to_f() / 100
 
@@ -32,6 +35,7 @@ loop do # main loop
   loop do
     prompt("Next, how many months will the loan be for?")
     loan_duration_in_months = gets().chomp()
+    
     if number?(loan_duration_in_months)
       break
     else
@@ -39,16 +43,18 @@ loop do # main loop
     end
   end
 
+# CALCULATING USER INPUT
   float_loan_duration_in_months = loan_duration_in_months.to_f()
-  
   monthly_interest_rate = monthly_interest_rate(apr,loan_duration_in_months.to_f)
   
+  ### REFACTOR LOGIC INTO SEPARATE METHOD(S)
   monthly_payment = loan_amount * ( monthly_interest_rate.round(4) / ( 1 - ( 1 + monthly_interest_rate.round(4) )**(-float_loan_duration_in_months)))
 
   total_interest_paid = (monthly_payment * float_loan_duration_in_months) - loan_amount
 
   total_including_interest = (total_interest_paid + loan_amount)
-
+  ###
+  
   final_output_prompt = <<-MSG
   Thanks! Based on your input here are your totals:
   Monthly Payments: $#{monthly_payment.round(2)}
