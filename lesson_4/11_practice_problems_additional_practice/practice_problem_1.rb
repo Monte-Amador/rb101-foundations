a0a5f1b5d9801eb11bc772bd1ee5404a1c3aa306
Monte Amador <monteamador@me.com>
2021-03-12 08:12:49 -0800

    initialize practice problems 2

---
+++ lesson_4/11_practice_problems_additional_practice/practice_problem_1.rb
@@ -0,0 +1,27 @@
+#[Launch School - an online school for Software Engineers](https://launchschool.com/lessons/85376b6d/assignments/a76c28ac)
+
+#### Practice Problem 1
+
+# Given the array below
+
+# `flintstones = ["Fred", "Barney", "Wilma", "Betty", "Pebbles", "BamBam"]`
+
+# Turn this array into a hash where the names are the keys and the values are the positions in the array.
+
+flintstones = ["Fred", "Barney", "Wilma", "Betty", "Pebbles", "BamBam"]
+# => {"Fred" => 0, "Barney" => 1, "Wilma" => 2, "Betty" => 3, "Pebbles" => 4,
+# "BamBam" => 5 }
+
+# get index position for each value
+# iterate through array and assign each item as a key and its index as the
+# value.
+# - each_with_object({}) do |value, hash
+# - loop with counter that increases and breaks when same size reaches
+# array.size
+# - each_with_index gives both the value and index through each iteration. 
+
+new_hash = {}
+flintstones.each_with_index do |value, index|
+  new_hash[value] = index
+end
+
---
+++ lesson_4/11_practice_problems_additional_practice/practice_problem_2.rb
@@ -0,0 +1,13 @@
+# [Launch School - an online school for Software Engineers](https://launchschool.com/lessons/85376b6d/assignments/a76c28ac)
+
+#### Practice Problem 2
+
+# Add up all of the ages from the Munster family hash:
+ages = { "Herman" => 32, "Lily" => 30, "Grandpa" => 5843, "Eddie" => 10, "Marilyn" => 22, "Spot" => 237 }
+
+sum_of_ages = 0
+ages.each do |_key, value|
+  sum_of_ages += value
+end
+
+sum_of_ages
